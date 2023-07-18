d836f046665dec90aa3adecb151e8766bf63a222
Sathya Babu <sathyaBabu@users.noreply.github.com>
2023-07-18 19:30:46 +0530

    Lambda mutable

---
+++ GenericLambdaMutable2/GenericLambdaMutable2/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  GenericLambdaMutable2
+//
+//  Created by Sathya Babu on 09/10/21.
+//
+
+#include <iostream>
+int main()
+{
+  int ammo{ 10 };
+
+  auto shoot{
+    // Added mutable after the parameter list.
+    [ammo]() mutable {
+      // We're allowed to modify ammo now
+      --ammo;
+
+      std::cout << "Pew! " << ammo << " shot(s) left.\n";
+    }
+  };
+
+  shoot();
+  shoot();
+
+ // std::cout << ammo << " shot(s) left\n";
+
+  return 0;
+}
